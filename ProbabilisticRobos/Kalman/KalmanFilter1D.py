34d07e2f5f1c694ea9588b0aa8fe0286ab235da0
PhobicRemnant <sergio.m.gonzalez.a.94@gmail.com>
2020-10-28 00:14:57 +0100

    Working linear Kalman Filter.

--- ProbabilisticRobos/Kalman/KalmanFilter1D.py
+++ ProbabilisticRobos/Kalman/KalmanFilter1D.py
@@ -1,4 +1,6 @@
 import numpy as np
+from numpy.linalg import inv
+import matplotlib.pyplot as plt
 
 """
 
@@ -10,6 +12,8 @@ The only difference with the algorithm shown in the table and this implementatio
 mean calculation, the prediction will be done with a state vector of the previous state.
 
 """
+
+
 
 
 
@@ -28,6 +32,7 @@ dt = t_end/iterations
 # Define predicted and calculated position arrays for plotting purposes
 x_true = np.zeros((time.size,2))
 x_pred = np.zeros(time.shape)
+x_meassured = np.zeros(time.shape)
 x_filtered = np.zeros(time.shape)
 
 # Define initial variables
@@ -43,41 +48,59 @@ A = np.array([[1, dt], [0 ,1]])
 # Matrix B - For the control vector
 B = np.array([ [(dt**2)/2], [dt] ])
 # Matrix C - Meassurement matrix
-C = np.array([0,1])
+C = np.array([1,0])
 # Q matrix - covariance matrix
-sigma_model_x = 0.25    # Position model error variance
-sigma_modal_v = 0.25    # Velocity model error variance
+sigma_model_x = 0.35    # Position model error variance
+sigma_modal_v = 0.35    # Velocity model error variance
 Q = np.array([[ sigma_model_x**2, 0],[0,sigma_modal_v**2 ]])
 
 # P matrix - initial process covariance matrix
 P = np.eye(2)
 # R matrix - meassurement covariance 
-sigma_meas = 0.3
+sigma_meas = 1
 R = sigma_meas**2
-# Current state vector
-state = np.array([[0],[v0]])
 
 # Control vector
 u = np.array([a0])
 
-"""
-# 1 iteration of a Linear Kalman Filter
-"""
-# --------------------------
-# 1.- Sensor data simulation
-# --------------------------
-# Get the true value of the current velocity and add noise from a random number of a standar deviation
-# Multiply sigma_meas with a random number from the standard distribuition to change sigma=1 for sigma_meas
-z = np.dot( np.transpose(C), x_true[0]) + sigma_meas*np.random.randn(1)
-# -------------------------
-# 2.- Next State prediction
-# -------------------------
-# Calculate the current state prediction 
-xPred = np.dot(A,state) + np.dot(B,u)
-# Calculate the predicted covariance of the process
-PPred = np.dot( np.dot(A,P), np.transpose(A) ) + Q
-# -------------------------------
-# 3.- Kalman constant update
-# -------------------------------
-K = np.dot(PPred,np.transpose(C))
-k = np.linalg.inv( np.dot( np.dot(C,PPred), np.transpose(C)) ) 
+for i in range(1,iterations):
+    """
+    # 1 iteration of a Linear Kalman Filter
+    """
+    
+    # -------------------------
+    # 1.- Next State prediction
+    # -------------------------
+    # Calculate the current state prediction 
+    xPred = np.dot(A,x_true[i-1]) + np.dot(B,u)
+    xPred = xPred.reshape(xPred.shape[0],1)
+    x_true[i] = np.transpose(xPred)
+    x_pred[i] = xPred[0]
+    # Calculate the predicted covariance of the process
+    PPred = np.dot( np.dot(A,P), np.transpose(A) ) + Q
+    # --------------------------
+    # 2.- Sensor data simulation
+    # --------------------------
+    # Get the true value of the current velocity and add noise from a random number of a standar deviation
+    # Multiply sigma_meas with a random number from the standard distribuition to change sigma=1 for sigma_meas
+    z = np.dot( np.transpose(C), x_true[i]) + sigma_meas*np.random.randn(1)
+    x_meassured[i] = z
+    # -------------------------------
+    # 3.- Kalman constant update
+    # -------------------------------
+    K = np.dot(PPred, np.transpose(C))
+    # Reshape array to avoid an issue with (2,) results
+    K = K.reshape(K.shape[0],1)
+    K = np.dot(K, inv( np.array([[ np.dot( np.dot(C,PPred), np.transpose(C) ) + R ]]) ) ) 
+    # Calculate correction based on prediction and meassurement
+    xEst = xPred + K*(z[0] - np.dot(C,xPred))
+    x_filtered[i] = xEst[0]
+    # Calculate new process variance matrix
+    P = np.dot(np.eye(2) - np.dot(C,K),PPred)
+
+
+plt.plot(x_pred[0:len(x_pred):50],'r--', x_meassured[0:len(x_meassured):50], 'bs',x_filtered[0:len(x_filtered):50],'g^')
+plt.title("Linear Kalman Filter")
+plt.ylabel("Position(m)")
+plt.xlabel("Time(s)")
+plt.show()
